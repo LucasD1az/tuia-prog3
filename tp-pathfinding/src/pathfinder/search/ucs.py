4108e44db15fd9f1ce7dcf9f6783802d08a2ea16
FacuMagliaro <facu.magliaro22@gmail.com>
2026-04-08 19:10:03 -0300

    Subo algoritmo UCS

--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -22,8 +22,42 @@ class UniformCostSearch:
         reached = {}
         reached[root.state] = root.cost
 
-        # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        # Apply objective test
+        if grid.objective_test(root.state):
+            return Solution(root, reached)
+        
 
-        return NoSolution(reached)
+        # Initialize frontier with the root node
+        frontier = PriorityQueueFrontier()
+        frontier.add(root, root.cost)
+
+        while not frontier.is_empty():
+            
+            node = frontier.pop()
+
+            if grid.objective_test(node.state):
+                return Solution(node, reached)
+            
+            for action in grid.actions(node.state):
+                #Get successor
+                successor = grid.result(node.state, action)
+                #Calculate the total cost
+                cost = node.cost + grid.individual_cost(node.state, action)
+                
+                # Check if the successor is reached
+                if successor not in reached or cost < reached[successor]:
+                    # Initialize the son node 
+                    son = Node(
+                    "",
+                    successor,
+                    cost,
+                    parent=node,
+                    action=action,
+                    )
+                    
+                
+                    reached[successor] = cost
+
+                    frontier.add(son, son.cost)
+                
+        return NoSolution(reached) 
