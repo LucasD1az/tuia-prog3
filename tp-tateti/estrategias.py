8b38a9068a23cbf4c3490596c22fab7266aed711
Lucas Diaz Celauro <lucasd81@gmail.com>
2026-04-17 11:22:12 -0300

    Completado algoritmo minimax

--- tp-tateti/estrategias.py
+++ tp-tateti/estrategias.py
@@ -41,34 +41,51 @@ def estrategia_minimax(tateti: Tateti, estado: List[List[str]]) -> Tuple[int, in
         
     Returns:
         Tuple[int, int]: Acción elegida (fila, columna)
-        
-    Raises:
-        NotImplementedError: Hasta que el alumno implemente el algoritmo
     """
-    def MINIMAX_MAX(tateti: Tateti, estado: List[List[str]])-> Tuple[int, int]:
-    
-    if tateti.test_terminal(estado):
-        return tateti.utilidad(estado, JUGADOR_MAX) 
-    
-    valor = -10000
 
-    for accion in tateti.acciones(estado):
-        #Obtener el sucesor
-        sucesor = tateti.resultado(estado, accion)
-        valor = max(valor, MINIMAX_MIN(tateti, sucesor))
+    # Definimos las funciones recursivas para calcular los valores minimax de los nodos
+    def MINIMAX_MAX(tateti: Tateti, estado: List[List[str]])-> Tuple[int, int]:
+        # Calcula recursivamente el valor minimax
+        # en un nodo MAX
+    
+        # Caso base: si el juego terminó, devuelve el valor del tablero
+        if tateti.test_terminal(estado):
+            return tateti.utilidad(estado, JUGADOR_MAX) 
+    
+        valor = -10000 # Un valor muy negativo que simula "-infinito"
+        
+        # En un nodo MAX buscamos maximizar su ganancia eligiendo el valor más alto de sus hijos (MIN)
+        for accion in tateti.acciones(estado):
+            #Obtener el sucesor
+            sucesor = tateti.resultado(estado, accion)
+            valor = max(valor, MINIMAX_MIN(tateti, sucesor))
         return valor 
 
     
     def MINIMAX_MIN(tateti: Tateti, estado: List[List[str]])-> Tuple[int, int]:
-    
-    if tateti.test_terminal(estado):
-        return tateti.utilidad(estado, JUGADOR_MAX) 
-    
-    valor = 10000
+        # Calcula recursivamente el valor minimax
+        # en un nodo MIN
 
-    for accion in tateti.acciones(estado):
-        #Obtener el sucesor
-        sucesor = tateti.resultado(estado, accion)
-        valor = min(valor, MINIMAX_MAX(tateti, sucesor))
+        # Caso base: si el juego terminó, devuelve el valor del tablero
+        if tateti.test_terminal(estado):
+            return tateti.utilidad(estado, JUGADOR_MAX) 
+        
+        valor = 10000 # Un valor muy alto que simula "+infinito"
+        
+        # En un nodo MIN se busca minimizar la ganancia de MAX eligiendo el valor más bajo de sus hijos (MAX)
+        for accion in tateti.acciones(estado):
+            #Obtener el sucesor
+            sucesor = tateti.resultado(estado, accion)
+            valor = min(valor, MINIMAX_MAX(tateti, sucesor))
         return valor
     
+    # Si es el turno de MAX, evalúa todas las acciones posibles y elige la que maximiza el resultado
+    if tateti.jugador(estado) == JUGADOR_MAX:
+        sucesores = {accion: MINIMAX_MIN(tateti, tateti.resultado(estado,accion)) 
+                     for accion in tateti.acciones(estado)}
+        return max(sucesores, key=sucesores.get) # Obtiene la clave (accion) con el mayor valor
+    
+    if tateti.jugador(estado) == JUGADOR_MIN:
+        sucesores = {accion: MINIMAX_MAX(tateti, tateti.resultado(estado,accion)) 
+                     for accion in tateti.acciones(estado)}
+        return min(sucesores, key=sucesores.get) # Obtiene la clave (accion) con el menor valor
